6a52555c7485e205e49cbbf9d9f37e65f731fd75
James Keicher <jamesdkeicher@gmail.com>
2022-05-25 11:20:37 -0700

    feat: data context and hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,31 @@ export default function App() {
   return (
     <>
       <Header />
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/watch">
-          <Watch />
-        </Route>
-        <Route path="/profile/:id">
-          <Profile />
-        </Route>
-        <Route path="/profile/:id/edit">
-          <EditProfile />
-        </Route>
-        <Route path="/upload/">
-          <Upload />
-        </Route>
-        <Route path="/upload/edit/:id">
-          <EditUpload />
-        </Route>
-        <Route path="profile/history">
-          <EditUpload />
-        </Route>
-      </Switch>
+      <DataProvider>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/watch">
+            <Watch />
+          </Route>
+          <Route path="/profile/:id">
+            <Profile />
+          </Route>
+          <Route path="/profile/:id/edit">
+            <EditProfile />
+          </Route>
+          <Route path="/upload/">
+            <Upload />
+          </Route>
+          <Route path="/upload/edit/:id">
+            <EditUpload />
+          </Route>
+          <Route path="profile/history">
+            <EditUpload />
+          </Route>
+        </Switch>
+      </DataProvider>
     </>
   );
 }
