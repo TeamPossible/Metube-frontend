0ed16b7aeada63e32b60765b6144b23b9a8812ef
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-26 11:50:35 -0700

    added secrets to .env and yml file

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -3,14 +3,21 @@ import { useState } from 'react';
 export const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password);
   };
 
-  return (
-    <>
+  const handleSignUp = (e) => {
+    
+  }
+
+  let content; 
+
+  hasAccount ? content = (<>
       <form onSubmit={handleSubmit}>
         <legend>Sign In</legend>
         <input
@@ -28,7 +35,38 @@ export const Auth = () => {
           onChange={(e) => setPassword(e.target.value)}
         ></input>
         <button>Submit</button>
+        <button onClick={() => setHasAccount(false)}>Don't have an account?</button>
       </form>
-    </>
+    </>) : content = (<>
+      <form onSubmit={handleSubmit}>
+        <legend>Sign Up</legend>
+        <input
+          placeholder="email"
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <input
+          placeholder="username"
+          type="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <button>Submit</button>
+        <button onClick={() => setHasAccount(true)}>Already have an account?</button>
+      </form>
+    </>)
+
+  return (
+    {content}
   );
 };
