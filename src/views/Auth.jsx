b5291d4e18208a101fe1efff38579fa3101b462c
Kyra Christensen <89879672+Kyra-christensen@users.noreply.github.com>
2022-05-26 14:39:54 -0700

    Dev (#2)
    * added secrets to .env and yml file
    
    * created fetch utils functions
    
    * got auth working

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react';
+import { signUp, signIn } from '../utils/fetch-utils';
 
 export const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password);
   };
 
-  return (
-    <>
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await signUp({ username, password, email });
+      console.log('****response****', res);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  let content; 
+
+  hasAccount ? content = (<>
       <form onSubmit={handleSubmit}>
         <legend>Sign In</legend>
         <input
@@ -28,7 +42,38 @@ export const Auth = () => {
           onChange={(e) => setPassword(e.target.value)}
         ></input>
         <button>Submit</button>
+        <button onClick={() => setHasAccount(false)}>Don't have an account?</button>
       </form>
-    </>
+    </>) : content = (<>
+      <form onSubmit={handleSignUp}>
+        <legend>Sign Up</legend>
+        <input
+          placeholder="email"
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <input
+          placeholder="username"
+          type="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <button>Submit</button>
+        <button onClick={() => setHasAccount(true)}>Already have an account?</button>
+      </form>
+    </>)
+
+  return (
+    content
   );
 };
