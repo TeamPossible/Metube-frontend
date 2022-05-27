3e5c7476ff34e6b81b39304688d6e42f8f4fae07
James Keicher <jamesdkeicher@gmail.com>
2022-05-27 11:01:56 -0700

    feat: multiple videos can be uploaded at a time

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -5,19 +5,24 @@ import { useAuth } from '../hooks/useAuth';
 export const Upload = () => {
   const { user } = useAuth();
   console.log('USER', user);
-  const [media, setMedia] = useState('');
+  const [media, setMedia] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(media);
-    const res = await uploadVideo(user.id, media);
-    console.log('UPLOAD', res);
+    media.map(async (video) => {
+      return await uploadVideo(user.id, video);
+    });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <legend>Upload</legend>
-      <input type="file" onChange={(e) => setMedia(e.target.files[0])}></input>
+      <input
+        type="file"
+        multiple
+        onChange={(e) => setMedia(Array.from(e.target.files))}
+      ></input>
       <button>Upload Video</button>
     </form>
   );
