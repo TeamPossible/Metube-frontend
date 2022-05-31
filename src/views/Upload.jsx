a8eecead22eff9a9d575f74c57d10519fa384965
James Keicher <jamesdkeicher@gmail.com>
2022-05-31 12:27:48 -0700

    feat: added inputs for video title and description to media upload form. values currently unused

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -5,6 +5,8 @@ import { useAuth } from '../hooks/useAuth';
 export const Upload = () => {
   const { user } = useAuth();
   const [media, setMedia] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   const handleSubmit = async (e) => {
     console.log(user);
@@ -18,11 +20,28 @@ export const Upload = () => {
   return (
     <form onSubmit={handleSubmit}>
       <legend>Upload</legend>
+      <label htmlFor="media">Select Video</label>
       <input
         type="file"
+        name="media"
         multiple
         onChange={(e) => setMedia(Array.from(e.target.files))}
       ></input>
+      <br></br>
+      <label htmlFor="description">Video Description</label>
+      <input
+        type="text"
+        name="description"
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <br></br>
+      <label htmlFor="title">Video Title</label>
+      <input
+        type="text"
+        name="title"
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <br></br>
       <button>Upload Video</button>
     </form>
   );
