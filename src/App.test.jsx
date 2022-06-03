de069c3f52babcf1a8b454b399d4969a2101d547
James Keicher <jamesdkeicher@gmail.com>
2022-06-03 09:56:14 -0700

    helps to actually put a test in the test file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,1 +1,3 @@
-describe("It should exist purely to have a test that doesn't throw an error", () => {});
+describe("It should exist purely to have a test that doesn't throw an error", () => {
+  it('Should see me, not fail, then give a green check', () => {});
+});
